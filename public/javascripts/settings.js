ca1977d047ee0c83ccb8f76e6885b03ac5755743
Tanush Reddy <atanushreddy@gmail.com>
2024-05-01 16:53:15 +0530

    settings modified

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -26,6 +26,36 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+var blurContainer = document.querySelector('.full')
+var container = document.querySelector('.confirmation')
+var buttons = document.querySelector('.buttons')
+
+function showLogout(){
+    blurContainer.style.display = 'flex';
+    setTimeout(()=> {
+        container.style.height = '150px';
+        container.style.width = '350px';
+    }, 0)
+    setTimeout(()=>{
+        buttons.style.display = 'flex';
+    }, 100)
+}
+
+function diableLogout(){
+    container.style.height = '0';
+    container.style.width = '0';
+    buttons.style.display = 'none';
+    setTimeout(()=> {
+        blurContainer.style.display = 'none'
+    }, 100)
+}
+
+window.onload = () => {
+    document.querySelector('.full').onclick = diableLogout;
+    document.querySelector('.no').onclick = diableLogout;
+    document.getElementById('logout').onclick = showLogout;
+};
+
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     document.querySelector('.btn-submit').addEventListener('click', function() {
